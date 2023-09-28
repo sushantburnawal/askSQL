5859b2e03f326986a184532ba2be7a4751c98288
sushantburnawal <sushant1909@gmail.com>
2023-09-28 10:12:57 +0530

    first commit

---
+++ askSQL.py
@@ -0,0 +1,94 @@
+from vanna.local import LocalContext_OpenAI
+from utils import *
+import streamlit as st
+import streamlit_authenticator as stauth
+import pickle
+from pathlib import Path
+
+
+def ask_question(question,auto_train):
+    try:
+        sql = vn.ask(question=question, print_results=False)[0]
+    except Exception:
+        st.warning("OpenAI API timeout!!",icon="🚨" )
+        
+        
+    print(sql)
+    try:
+        df = run_sql_sql_server(sql)
+    except Exception as e:
+        print(e)
+        #Invalid line of questioning 
+        e2 = str(e)
+        error = "Invalid column name"
+        if error in e2:
+            strIndex = int(e2.index(error)) + len(error)
+            endIndex = int(e2.find('.'))
+            columnError = e2[strIndex:endIndex]
+            st.warning("The term " + columnError + "does not exist in the database" ,icon="🚨")
+        else :
+            #Any other database related error due to querying
+            st.warning("Could not execute your SQL query",icon="🚨")
+    if df is not None:
+        st.dataframe(df)
+        if len(df.index)>0 and auto_train:
+            vn.add_question_sql(question=question,sql=sql)
+            print("Training Data added successfully")
+    else:
+        st.write("There are no records for your SQL query in the database")
+
+
+# --- USER AUTHENTICATION ---
+names = ["Flatworld Solutions"]
+usernames = ["flatworldsolutions"]
+
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+credentials = {
+        "usernames":{
+            usernames[0]:{
+                "name":names[0],
+                "password":hashed_passwords[0]
+                }       
+            }
+        }
+
+authenticator = stauth.Authenticate(credentials,"askSQL_dashboard","abcdefg",cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
+    
+    authenticator.logout("Logout", "sidebar")
+    st.sidebar.title(f"Welcome {name}")
+
+    st.title("askSQL📃🔬")
+    api_key = st.text_input("OpenAI API Key:")
+    question_input = st.text_input("Ask a Question on the Database:")
+    if st.button("Submit Question"):
+        
+        #question_input = st.text_input("Ask a Question on the Database:")
+        if is_openai_api_key_valid(api_key):
+            vn = LocalContext_OpenAI({"api_key": api_key})
+            if question_input!="":
+                
+                with st.spinner("Searching. Please hold..."):
+
+                    try:
+                        ask_question(question=question_input,auto_train=False)
+                    except Exception as e:
+                        print(e)
+            else:
+                st.warning("Question cannot be empty",icon="⚠️")
+        else :
+            st.warning("Invalid API Key",icon="⚠️")
+
---
+++ utils.py
@@ -0,0 +1,60 @@
+#from vanna.local import LocalContext_OpenAI
+import pymssql
+import pandas as pd
+import time
+import random
+import requests
+
+def is_openai_api_key_valid(api_key):
+    """
+    Check if an OpenAI API key is valid by making a simple request to the API.
+
+    Args:
+        api_key (str): The OpenAI API key to be checked.
+
+    Returns:
+        bool: True if the API key is valid, False otherwise.
+    """
+    # Define the API endpoint you want to test
+    api_endpoint = "https://api.openai.com/v1/models"
+
+    # Set up the request headers with the API key
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    try:
+        # Make a GET request to the API
+        print(1)
+        response = requests.get(api_endpoint, headers=headers)
+        print(response)
+        print(2)
+        # Check the response status code
+        if response.status_code == 200:
+            return True  # API key is valid
+        else:
+            return False  # API key is invalid
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return False  # An error occurred, assume the key is invalid
+    
+def connect_to_database(**kwargs):
+    for i in range(5):
+        try:
+            return pymssql.connect(**kwargs)
+        except :
+            wait_time = (2 ** i) + random.random()
+            time.sleep(wait_time)
+    raise Exception("Not able to connect to database, even after hitting max retries")
+
+def run_sql_sql_server(sql: str):
+    
+    conn_sql_server = connect_to_database(host="sqlgpt.database.windows.net",user="sushant@sqlgpt",password="Solarsystem$123",database="sqlgpt")
+    # try:
+    df = pd.read_sql(sql, conn_sql_server)
+    return df
+    # except Exception as e:
+    #     print(e)
+        
