41c274ccbfdf3bf680d27d41e35b510a62351f52
sushantburnawal <sushant1909@gmail.com>
2023-09-28 12:19:41 +0530

    removed auth

--- askSQL.py
+++ askSQL.py
@@ -43,56 +43,56 @@ def ask_question(question,auto_train):
 
 
 # --- USER AUTHENTICATION ---
-names = ["Flatworld Solutions"]
-usernames = ["flatworldsolutions"]
+# names = ["Flatworld Solutions"]
+# usernames = ["flatworldsolutions"]
 
-# load hashed passwords
-file_path = Path(__file__).parent / "hashed_pw.pkl"
-with file_path.open("rb") as file:
-    hashed_passwords = pickle.load(file)
+# # load hashed passwords
+# file_path = Path(__file__).parent / "hashed_pw.pkl"
+# with file_path.open("rb") as file:
+#     hashed_passwords = pickle.load(file)
 
-credentials = {
-        "usernames":{
-            usernames[0]:{
-                "name":names[0],
-                "password":hashed_passwords[0]
-                }       
-            }
-        }
+# credentials = {
+#         "usernames":{
+#             usernames[0]:{
+#                 "name":names[0],
+#                 "password":hashed_passwords[0]
+#                 }       
+#             }
+#         }
 
-authenticator = stauth.Authenticate(credentials,"askSQL_dashboard","abcdefg",cookie_expiry_days=30)
+# authenticator = stauth.Authenticate(credentials,"askSQL_dashboard","abcdefg",cookie_expiry_days=30)
 
-name, authentication_status, username = authenticator.login("Login", "main")
+# name, authentication_status, username = authenticator.login("Login", "main")
 
-if authentication_status == False:
-    st.error("Username/password is incorrect")
+# if authentication_status == False:
+#     st.error("Username/password is incorrect")
 
-if authentication_status == None:
-    st.warning("Please enter your username and password")
+# if authentication_status == None:
+#     st.warning("Please enter your username and password")
 
-if authentication_status:
+# if authentication_status:
     
-    authenticator.logout("Logout", "sidebar")
-    st.sidebar.title(f"Welcome {name}")
+    # authenticator.logout("Logout", "sidebar")
+    # st.sidebar.title(f"Welcome {name}")
 
-    st.title("askSQL📃🔬")
-    api_key = st.text_input("OpenAI API Key:")
-    question_input = st.text_input("Ask a Question on the Database:")
-    if st.button("Submit Question"):
-        
-        #question_input = st.text_input("Ask a Question on the Database:")
-        if is_openai_api_key_valid(api_key):
-            vn = LocalContext_OpenAI({"api_key": api_key})
-            if question_input!="":
-                
-                with st.spinner("Searching. Please hold..."):
+st.title("askSQL📃🔬")
+api_key = st.text_input("OpenAI API Key:")
+question_input = st.text_input("Ask a Question on the Database:")
+if st.button("Submit Question"):
+    
+    #question_input = st.text_input("Ask a Question on the Database:")
+    if is_openai_api_key_valid(api_key):
+        vn = LocalContext_OpenAI({"api_key": api_key})
+        if question_input!="":
+            
+            with st.spinner("Searching. Please hold..."):
 
-                    try:
-                        ask_question(question=question_input,auto_train=False)
-                    except Exception as e:
-                        print(e)
-            else:
-                st.warning("Question cannot be empty",icon="⚠️")
-        else :
-            st.warning("Invalid API Key",icon="⚠️")
+                try:
+                    ask_question(question=question_input,auto_train=False)
+                except Exception as e:
+                    print(e)
+        else:
+            st.warning("Question cannot be empty",icon="⚠️")
+    else :
+        st.warning("Invalid API Key",icon="⚠️")
 
