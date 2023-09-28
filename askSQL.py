fb03c8e7ba102389ea658308618e89e2ed60c304
sushantburnawal <sushant1909@gmail.com>
2023-09-28 13:15:37 +0530

    auth added

--- askSQL.py
+++ askSQL.py
@@ -8,7 +8,7 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import pickle
 from pathlib import Path
-
+import hashlib
 
 def ask_question(question,auto_train):
     try:
@@ -74,25 +74,96 @@ def ask_question(question,auto_train):
     
     # authenticator.logout("Logout", "sidebar")
     # st.sidebar.title(f"Welcome {name}")
+def make_hashes(password):
+    	return hashlib.sha256(str.encode(password)).hexdigest()
+
+def check_hashes(password,hashed_text):
+	if make_hashes(password) == hashed_text:
+		return hashed_text
+	return False
+# DB Management
+import sqlite3 
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+# DB  Functions
+def create_usertable():
+	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+
+
+def add_userdata(username,password):
+	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+	conn.commit()
+
+def login_user(username,password):
+	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+	data = c.fetchall()
+	return data
+
+
+def view_all_users():
+	c.execute('SELECT * FROM userstable')
+	data = c.fetchall()
+	return data
 
 st.title("askSQL📃🔬")
-api_key = st.text_input("OpenAI API Key:")
-question_input = st.text_input("Ask a Question on the Database:")
-if st.button("Submit Question"):
-    
-    #question_input = st.text_input("Ask a Question on the Database:")
-    if is_openai_api_key_valid(api_key):
-        vn = LocalContext_OpenAI({"api_key": api_key})
-        if question_input!="":
-            
-            with st.spinner("Searching. Please hold..."):
 
-                try:
-                    ask_question(question=question_input,auto_train=False)
-                except Exception as e:
-                    print(e)
+menu = ["Home","Login"]
+choice = st.sidebar.selectbox("Menu",menu)
+
+if choice == "Home":
+    st.subheader("Home Screen")
+    st.write('Go log in yourself !!')
+    
+
+elif choice == "Login":
+    
+
+    username = st.sidebar.text_input("User Name")
+    password = st.sidebar.text_input("Password",type='password')
+    if st.sidebar.button("Login"):
+        # if password == '12345':
+        create_usertable()
+        hashed_pswd = make_hashes(password)
+
+        result = login_user(username,check_hashes(password,hashed_pswd))
+        if result:
+
+            st.subheader("You have logged in !!")
+            api_key = st.text_input("OpenAI API Key:")
+            question_input = st.text_input("Ask a Question on the Database:")
+            if st.button("Submit Question"):
+                
+                #question_input = st.text_input("Ask a Question on the Database:")
+                if is_openai_api_key_valid(api_key):
+                    vn = LocalContext_OpenAI({"api_key": api_key})
+                    if question_input!="":
+                        
+                        with st.spinner("Searching. Please hold..."):
+
+                            try:
+                                ask_question(question=question_input,auto_train=False)
+                            except Exception as e:
+                                print(e)
+                    else:
+                        st.warning("Question cannot be empty",icon="⚠️")
+                else :
+                    st.warning("Invalid API Key",icon="⚠️")
         else:
-            st.warning("Question cannot be empty",icon="⚠️")
-    else :
-        st.warning("Invalid API Key",icon="⚠️")
+            st.warning("Incorrect Username/Password")
+
+
+
+
+
+# elif choice == "SignUp":
+#     st.subheader("Create New Account")
+#     new_user = st.text_input("Username")
+#     new_password = st.text_input("Password",type='password')
+
+#     if st.button("Signup"):
+#         create_usertable()
+#         add_userdata(new_user,make_hashes(new_password))
+#         st.success("You have successfully created a valid Account")
+#         st.info("Go to Login Menu to login")
+
 
